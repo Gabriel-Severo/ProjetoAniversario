d0b8700185da2c4babaa640101cd2d27963cb69e
Gabriel Rodrigues <gabriel.ivi@hotmail.com>
2020-08-14 14:20:04 -0400

    Adicionado classe Aniversario

--- listaraniversario/ListarAniversario.java
+++ listaraniversario/ListarAniversario.java
@@ -1,28 +1,25 @@
 package listaraniversario;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.Period;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 public class ListarAniversario {
 
     public static void main(String[] args) {
-//        Date date = new Date();
-//        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Brazil/East"));
-//        cal.setTime(date);
-//        System.out.println(cal.get(Calendar.DAY_OF_MONTH) + " " + cal.get(Calendar.MONTH));
-        LocalDate dataAtual = LocalDate.now();
-        LocalDate dataAniversario = LocalDate.of(2001, 01, 17);
-        LocalDate dataRestante = LocalDate.of(dataAtual.getYear(), dataAniversario.getMonth(), dataAniversario.getDayOfMonth());
+//        LocalDate dataAtual = LocalDate.now();
+//        LocalDate dataAniversario = LocalDate.of(2001, 01, 17);
+//        LocalDate dataRestante = LocalDate.of(dataAtual.getYear(), dataAniversario.getMonth(), dataAniversario.getDayOfMonth());
+//        
+//        Period intervalo = Period.between(dataAtual, dataRestante);
+//        int idade = Period.between(dataAniversario, dataAtual).getYears();
+//        
+//        System.out.println(intervalo.getDays() + " " + intervalo.getMonths());
+//        System.out.println(idade);
+        Aniversario aniversario1 = new Aniversario();
+        aniversario1.setNome("Gabriel");
+        aniversario1.setDataAniversario(LocalDate.of(2001, 1, 17));
         
-        Period intervalo = Period.between(dataAtual, dataRestante);
-        int idade = Period.between(dataAniversario, dataAtual).getYears();
-        
-        System.out.println(intervalo.getDays() + " " + intervalo.getMonths());
-        System.out.println(idade);
+        System.out.println(aniversario1);
     }
     
 }
